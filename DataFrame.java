b3b976c4ab2ff9ad7d807243200f83c89400fe82
jbradlim08 <jbradlim1@hawk.iit.edu>
2024-04-24 17:56:51 -0500

    Update DataFrame.java

--- DataFrame.java
+++ DataFrame.java
@@ -6,59 +6,116 @@ public class DataFrame {
 
     private ArrayList<String> columnHeaders = new ArrayList<>();
     private ArrayList<String> columnDataTypes = new ArrayList<>();
-    private BufferedReader file = null;
+    private File file = null;
+    private BufferedReader br;
 
     public DataFrame() {
 
     }
 
     public List<String> getColumnHeaders() {
-        return Collections.unmodifiableList(columnHeaders);
+        return this.columnHeaders;
     }
 
-    public List<String> getColumnDataTypes() {
-        return Collections.unmodifiableList(columnDataTypes);
+    public List<String> getColumnDatatypes() {
+        return this.columnDataTypes;
     }
 
-    public BufferedReader getFile() {
-        return this.file;
-    }
+    public void setColumnHeadersandDatatypes() {
+        try (Scanner scanFile = new Scanner(this.file)) {
+            if (scanFile.hasNextLine()) {
+                String firstLine = scanFile.nextLine();
+                String[] headers = firstLine.split(",");
 
-    public void setColumnHeaders() {
-        try {
-            Scanner scanFile = new Scanner(file);
-            while (scanFile.hasNext()) {
+                // Add the headers to the list
+                for (String header : headers) {
+                    columnHeaders.add(header);
+                }
 
+                if (scanFile.hasNextLine()) {
+                    String secondLine = scanFile.nextLine();
+                    String[] datatypes = secondLine.split(",");
+
+                    for (String datatype : datatypes) {
+                        columnDataTypes.add(datatype);
+                    }
+                }
+            } else {
+                System.out.println("File is empty.");
             }
         } catch (FileNotFoundException fne) {
-            System.out.println(fne);
+            System.out.println("File not found: ");
+        } catch (Exception e) {
+            System.out.println("An error occurred while reading the file");
         }
 
+    }
+
+    public File getFile() {
+        return this.file;
     }
 
     public void importCSV() {
 
         Scanner scan = new Scanner(System.in);
-        try {
-            System.out.print("Enter Filename (without .csv): ");
-            String csv = scan.nextLine();
-            String importcsv = csv + ".csv";
+        boolean b = true;
+        System.out.print("Enter Filename (without .csv): ");
+        while (b) {
+            try {
 
-            file = new BufferedReader(new FileReader(importcsv));
+                String csv = scan.nextLine(); // try input
+                String importcsv = csv + ".csv";
 
-        } catch (FileNotFoundException fne) {
-            System.out.println("no such file");
+                file = new File(importcsv);
+                br = new BufferedReader(new FileReader(file));
+                b = false;
+
+                setColumnHeadersandDatatypes();
+            } catch (FileNotFoundException fne) {
+                System.out.println("no such file");
+                System.out.print("Enter Filename (without .csv): ");
+            }
         }
 
     }
 
-    public static double averageColumn(){
-        try{    
-            averageColumn();
-        }
-        catch(){
+    public void changeActiveCSV() {
 
-        }
     }
 
+    public double averageColumn() {
+        boolean b = true;
+        while (b) {
+            try {
+                System.out.println("Enter column name:");
+
+                Scanner scan = new Scanner(System.in);
+                String input = scan.nextLine();
+
+            } catch (InputMismatchException ime) {
+
+            }
+        }
+
+    }
+
+    public double minimumColumn() {
+        return;
+    }
+
+    public double maximumColumn() {
+
+    }
+
+    public List<Double> frequencyTable(String columnName) {
+
+    }
+
+    public List<String> subsetDataFrame(String conditions) {
+
+    }
+
+    public File exportToCSV(String fileName) {
+
+    }
 }
