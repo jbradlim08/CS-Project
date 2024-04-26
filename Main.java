fcc58937e6fe0339dadb2760aa2826d9aeaa092d
jbradlim08 <jbradlim1@hawk.iit.edu>
2024-04-26 00:19:51 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> a = new ArrayList<>();
+        DataFrame dataFrame = new DataFrame();
+        File file = new File("Divvy_Trips_July2013.csv");
 
         a.add("1");
         a.add("2");
@@ -13,9 +15,20 @@ public class Main {
         a.add("6");
         a.add("7");
 
-        System.out.println(a.indexOf("c"));
+        System.out.println(a.indexOf("ada"));
         System.out.println(a);
-        System.out.println(Double.parseDouble(a.get(3)));
+        double sum = 0;
+        List<String> columnData = dataFrame.getColumnDataFromColumn(file, 4);
+        dataFrame.setColumnHeadersandDatatypes(file);
 
+        for (int i = 2; i < columnData.size(); i++) {
+            double value = Double.parseDouble(columnData.get(i));
+            sum += value;
+        }
+        System.out.println("average: " + sum / a.size());
+
+        System.out.println(dataFrame.getColumnDatatypes());
+        System.out.println(dataFrame.getColumnDatatypes().get(1));
+        System.out.println(!(dataFrame.getColumnDatatypes().get(1).equals("int")));
     }
 }
