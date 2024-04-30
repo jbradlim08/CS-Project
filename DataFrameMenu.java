ccdf4b7d2b14138b5e90ee453c96293bc4f23735
jbradlim08 <jbradlim1@hawk.iit.edu>
2024-04-30 13:49:29 -0500

    Update DataFrameMenu.java

--- DataFrameMenu.java
+++ DataFrameMenu.java
@@ -3,8 +3,9 @@ import java.io.*;
 
 public class DataFrameMenu {
 
+    static DataFrame dataFrame = new DataFrame();
+
     public static void main(String[] args) {
-        DataFrame dataFrame = new DataFrame();
         boolean loop = true;
         Scanner scan = new Scanner(System.in);
 
@@ -43,7 +44,7 @@ public class DataFrameMenu {
                 if (dataFrame.getFile() == null) {
                     switch (choice) {
                         case "i":
-                            dataFrame.importCSV();
+                            importCSV();
                             break;
                         case "q":
                             loop = false;
@@ -55,22 +56,22 @@ public class DataFrameMenu {
                 } else {
                     switch (choice) {
                         case "i":
-                            dataFrame.importCSV();
+                            importCSV();
                             break;
                         case "c":
-                            dataFrame.changeActiveCSV();
+                            changeActiveCSV();
                             break;
                         case "a":
-                            dataFrame.numericCalculationColumn(choice);
+                            numericColumn(choice);
                             break;
                         case "m":
-                            dataFrame.numericCalculationColumn(choice);
+                            numericColumn(choice);
                             break;
                         case "x":
-                            dataFrame.numericCalculationColumn(choice);
+                            numericColumn(choice);
                             break;
                         case "f":
-                            dataFrame.numericCalculationColumn(choice);
+                            numericColumn(choice);
                             break;
                         case "s":
                             break;
@@ -91,4 +92,74 @@ public class DataFrameMenu {
             scan.close();
         }
     }
+
+    public static void importCSV() {
+        Scanner scan = new Scanner(System.in);
+        boolean b = true;
+        while (b) {
+            System.out.print("Enter Filename (without .csv): ");
+            String csv = scan.nextLine();
+            if (csv.equals("!")) {
+                break;
+            } else {
+                int a = dataFrame.getDataFrameList().size();
+                dataFrame.importCSV(csv);
+                if (dataFrame.getDataFrameList().size() != a) {
+                    b = false;
+                }
+            }
+        }
+    }
+
+    public static void changeActiveCSV() {
+        Scanner scan = new Scanner(System.in);
+        boolean b = true;
+
+        while (b) {
+            System.out.print("Change dataframe (press '!' to exit): ");
+            String choice = scan.nextLine();
+            if (choice.equals("!")) {
+                break;
+            } else {
+                File a = dataFrame.changeActiveCSV(choice);
+                if (a == dataFrame.getFile()) {
+                    b = false;
+                }
+            }
+        }
+    }
+
+    public static void numericColumn(String choice) {
+        Scanner scan = new Scanner(System.in);
+        boolean b = true;
+
+        while (b) {
+            System.out.print("Enter column name (press '!' to exit): ");
+            String columnName = scan.nextLine();
+
+            if (columnName.equals("!")) {
+                break;
+            } else {
+                double result = dataFrame.numericColumn(columnName, choice);
+                if (choice.equals("a")) {
+                    System.out.println(columnName + " average: " + result);
+                } else if (choice.equals("m")) {
+                    System.out.println(columnName + " minimum: " + result);
+                } else if (choice.equals("x")) {
+                    System.out.println(columnName + " maximum: " + result);
+                } else {
+                    if (result == 1) {
+                        System.out.println(columnName + " freq table");
+                        dataFrame.frequencyTable(dataFrame.minimumColumn(), dataFrame.maximumColumn());
+                    }
+                }
+
+                if (result != 0) { // if it succeeded, the loop stoped
+                    b = false;
+                }
+
+            }
+        }
+    }
+
 }
