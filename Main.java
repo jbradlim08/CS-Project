e6bb2c9f57c48051d2f8f6cfecb33dbdadbf080f
jbradlim08 <jbradlim1@hawk.iit.edu>
2024-05-01 11:35:28 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,56 +7,17 @@ public class Main {
         DataFrame dataFrame = new DataFrame();
         File file = new File("Divvy_Trips_July2013.csv");
 
-        a.add("1");
-        a.add("2");
-        a.add("3");
-        a.add("4");
-        a.add("5");
-        a.add("6");
-        a.add("7");
-
-        System.out.println(a.indexOf("ada"));
-        System.out.println(a);
-        double sum = 0;
-        List<String> columnData = dataFrame.getColumnDataFromColumn(4);
-        dataFrame.setColumnHeadersandDatatypes(file);
-
-        for (int i = 2; i < columnData.size(); i++) {
-            double value = Double.parseDouble(columnData.get(i));
-            sum += value;
-        }
-        System.out.println("average: " + sum / a.size());
-
-        System.out.println(dataFrame.getColumnDatatypes());
-        System.out.println(dataFrame.getColumnDatatypes().get(1));
-        System.out.println(!(dataFrame.getColumnDatatypes().get(1).equals("int")));
-
-        File addfile = new File("kkdfd.csv");
-        System.out.println(addfile);
-
-        int[] amount = { 0, 2, 3, 4, 5, 6, 7, 2 };
-        for (int i = 0; i < 3; i++) {
-            System.out.print("ok");
-            System.out.print("\n");
-        }
-        for (int i : amount) {
-            System.out.print(i + " ");
-        }
-
-        System.out.println(" ");
-        System.out.print("woke");
-        System.out.println(" ");
-        System.out.println("woke");
-
         try {
-
-            Scanner in = new Scanner(file);
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
-            System.out.println(in.nextLine());
+            Scanner scanFile = new Scanner(file);
+            System.out.println(scanFile.nextLine());
+            int i = 0;
+            while (i < 5) {
+                System.out.println(scanFile.nextLine());
+                i++;
+            }
         } catch (FileNotFoundException fnfe) {
-            System.out.println("..");
+            System.out.println(fnfe);
         }
     }
+
 }
