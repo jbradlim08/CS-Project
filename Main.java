4813470f3e3542bfab38e0d5cb0168aa37af4b68
jbradlim08 <jbradlim1@hawk.iit.edu>
2024-05-01 16:42:38 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,9 +6,11 @@ public class Main {
         ArrayList<String> a = new ArrayList<>();
         DataFrame dataFrame = new DataFrame();
         File file = new File("Divvy_Trips_July2013.csv");
+        System.out.println("pass");
 
         try {
             Scanner scanFile = new Scanner(file);
+            System.out.println("pass");
             System.out.println(scanFile.nextLine());
             int i = 0;
             while (i < 5) {
@@ -18,6 +20,19 @@ public class Main {
         } catch (FileNotFoundException fnfe) {
             System.out.println(fnfe);
         }
+        String newFile = "output.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(newFile));) {
+            Scanner scan = new Scanner(file);
+
+            bw.write(scan.nextLine() + "\n");
+            bw.write(scan.nextLine() + "\n");
+            bw.write(scan.nextLine() + "\n");
+
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+
     }
 
 }
